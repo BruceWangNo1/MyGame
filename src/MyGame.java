0846aaa22123045797eee60ed06c4bf24659dfb4
BruceWangNo1 <2819931971@qq.com>
2015-05-08 00:22:17 +0900

    Created Objects of each team

--- src/MyGame.java
+++ src/MyGame.java
@@ -1,9 +1,18 @@
+import Team.TeamClass;
+
 
 public class MyGame
 {
 	public static void main(String[] args)
 	{
 		//Hello Everyone, get working now
+		int[][][] counterparts = new int[10][10][20];
+		TeamClass Croatia = new TeamClass(1, "Croatia", "Croatia", 1, "Croatia");
+		TeamClass Czech = new TeamClass(2, "Czech", "Czech", 2, "Czech");
+		TeamClass Denmark = new TeamClass(3, "Denmark", "Denmark", 3, "Czech");
+		
+		
+		
 	}
 
 }
---
+++ src/Team/TeamClass.java
@@ -0,0 +1,19 @@
+package Team;
+
+public class TeamClass
+{
+	public int index;
+	public String name;
+	public String location;
+	public int ranking;
+	public String stadium;
+	
+	public TeamClass(int index, String name, String location, int ranking, String stadium)
+	{
+		this.index = index;
+		this.name = name;
+		this.location = location;
+		this.ranking = ranking;
+		this.stadium = stadium;
+	}
+}
